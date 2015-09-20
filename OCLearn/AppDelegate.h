9a740b2a5c4326f72276cb7b52275ac4e2bf2686
jlstmac <jlstmac@gmail.com>
2015-09-20 18:21:02 +0800

    Initial Commit

---
+++ OCLearn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCLearn
+//
+//  Created by LinShan Jiang on 20/9/15.
+//  Copyright (c) 2015年 LinShan Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCLearn
+//
+//  Created by LinShan Jiang on 20/9/15.
+//  Copyright (c) 2015年 LinShan Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
